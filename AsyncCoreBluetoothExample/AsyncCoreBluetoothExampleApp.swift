1dd516164f8c7390b097b055e854e77d70c20c20
sam <sam@meech-ward.me>
2023-08-21 10:05:24 -0700

    Initial Commit

---
+++ AsyncCoreBluetoothExample/AsyncCoreBluetoothExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncCoreBluetoothExampleApp.swift
+//  AsyncCoreBluetoothExample
+//
+//  Created by Sam Meech-Ward on 2023-08-21.
+//
+
+import SwiftUI
+
+@main
+struct AsyncCoreBluetoothExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
